f9c1e45a664a3af7433682aa40274c2f75a39a34
leonard-creater <smileme12369@gmail.com>
2024-11-16 16:19:14 +0000

    double check

---
+++ 24/10/2024/double check.py
@@ -0,0 +1,15 @@
+num=int(input("enter the number"))
+if num>50:
+    print("numberis greater than 50")
+
+    if num <50:
+        print("number is less than 50")
+
+    else:
+        print("it's an odd number")
+
+else:
+    print("it's an even number")
+
+
+
